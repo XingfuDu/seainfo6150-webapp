734783bea7e91a66f26b346a339858db65a34827
duxingfu <du.xingf@northeastern.edu>
2021-04-09 18:30:58 -0400

    add Adaptive layout assignment

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -1,31 +1,25 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styles from "./ArticleList.module.css";
-import ArticleListItem from "../ArticleListItem/ArticleListItem.jsx";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ArticleList.module.css';
+import ArticleListItem from '../ArticleListItem/ArticleListItem.jsx';
 
 const ArticleList = (props) => {
-  let displayContent;
+	let displayContent;
 
-  if (props.articles.length) {
-    displayContent = (
-      <ul className={styles.container}>
-        {props.articles.map((article) => (
-          <ArticleListItem article={article} key={article.slug} />
-        ))}
-      </ul>
-    );
-  } else {
-    displayContent = <div>You have no data!</div>;
-  }
+	if (props.articles.length) {
+		displayContent = (
+			<ul className={styles.container}>
+				{props.articles.map((article) => <ArticleListItem article={article} key={article.slug} />)}
+			</ul>
+		);
+	} else {
+		displayContent = <div>You have no data!</div>;
+	}
 
-  return (
-    <div>
-      {displayContent}
-    </div>
-  );
+	return <div>{displayContent}</div>;
 };
 
 ArticleList.propTypes = {
-    articles: PropTypes.array.isRequired
-}
+	articles: PropTypes.array.isRequired
+};
 export default ArticleList;
